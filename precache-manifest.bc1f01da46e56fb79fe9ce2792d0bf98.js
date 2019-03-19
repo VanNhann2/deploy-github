511ff952d6bb4f0d83b2cea60a8083d2b9362f5c
vannhan <nhanitdhsp@gmail.com>
2019-03-19 21:03:13 +0700

    Updates

---
+++ precache-manifest.bc1f01da46e56fb79fe9ce2792d0bf98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "87892681245f3623743c",
+    "url": "/deploy-github/static/css/main.fc4d725a.chunk.css"
+  },
+  {
+    "revision": "87892681245f3623743c",
+    "url": "/deploy-github/static/js/main.149c223f.chunk.js"
+  },
+  {
+    "revision": "0d6c236a0ecafc366e98",
+    "url": "/deploy-github/static/js/runtime~main.94aeffc2.js"
+  },
+  {
+    "revision": "f5ec782af55319af3bcc",
+    "url": "/deploy-github/static/js/2.869d436c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-github/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "104c219d3a57017e2b3e8502cf5403a9",
+    "url": "/deploy-github/index.html"
+  }
+];
